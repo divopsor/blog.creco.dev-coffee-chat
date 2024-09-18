c8a2062d17375d2e013521784bdd11310cc09d01
divopsor <divopsor@gmail.com>
2024-09-18 12:46:48 +0900

    update to client render

--- app/[route]/page.tsx
+++ app/[route]/page.tsx
@@ -1,17 +1,24 @@
-// Next.js 접근 경로 중 마지막 경로가 schedule 로 시작하면 /coffee-chat/schedule 로 리다이렉트
-import { redirect } from 'next/navigation';
 
-export default function RoutePage({ params }: { params: { route: string } }) {
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+interface RoutePageProps {
+  params: {
+    route: string;
+  };
+}
+
+export default function RoutePage({ params }: RoutePageProps) {
+  const router = useRouter();
   const { route } = params;
 
-  if (route.startsWith('schedule')) {
-    redirect('/schedule');
-  }
+  useEffect(() => {
+    if (route.startsWith('schedule')) {
+      router.replace('/schedule');
+    }
+  }, [route, router]);
 
-  return (
-    <div>
-      {/* 해당 route에 대한 콘텐츠 */}
-      <h1>Route: {route}</h1>
-    </div>
-  );
+  return null;
 }
